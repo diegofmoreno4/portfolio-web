8835890fec3e552f578e904a3ab5a5d3efabeadc
diegofmoreno4 <dfmm17ykty@gmail.com>
2023-10-23 07:58:19 -0500

    Actualización del componente HeroSection y Navbar

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,62 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
+
+const HeroSection = () => {
+  return (
+    <section>
+      <div className="grid grid-cols-1 lg:grid-cols-12">
+        <div className="col-span-7 place-self-center text-center sm:text-left">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+              Hola, soy {""}
+            </span>
+            <br />
+            <TypeAnimation
+              sequence={[
+                "Diego",
+                1000,
+                "Web Developer",
+                1000,
+                "Front-End Developer",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
+          </h1>
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+          Soy un entusiasta de las tecnologías con una profunda pasión por la innovación y la creación de soluciones. Me emociona colaborar en proyectos desafiantes y trabajar en equipos multidisciplinarios, siempre en busca de la mejora constante. Mi enfoque principal es proporcionar soluciones tecnológicas eficientes y de alta calidad, con un enfoque especial en la usabilidad, la accesibilidad y la experiencia del usuario.
+          </p>
+          <div>
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
+              Contáctame
+            </button>
+            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3">
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Descargar CV
+              </span>
+            </button>
+          </div>
+        </div>
+        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+          <div className="rounded-full bg-[#181818] w-[300px] h-[300px] relative">
+            <Image
+              src="/Images/Image-profile.jpg"
+              alt="Image Profile"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-full"
+              width={250}
+              height={250}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+ 
+
+export default HeroSection;
